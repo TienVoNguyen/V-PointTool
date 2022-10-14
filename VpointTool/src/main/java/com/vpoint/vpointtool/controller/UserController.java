914dfb843b2f1125b3e2a5ba8d771bf74ec58448
CanhNguyen <leagueyasuo186@gmail.com>
2022-10-14 09:45:59 +0700

    j

--- VpointTool/src/main/java/com/vpoint/vpointtool/controller/UserController.java
+++ VpointTool/src/main/java/com/vpoint/vpointtool/controller/UserController.java
@@ -1,4 +1,9 @@
 package com.vpoint.vpointtool.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Controller
+@CrossOrigin("*")
 public class UserController {
 }
