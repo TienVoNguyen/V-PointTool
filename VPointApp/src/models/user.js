2bb959c5528e0c9ffa840398cba763bc34aeebe9
TienVoNguyen <vontph18623@fpt.edu.vn>
2022-10-27 15:08:20 +0700

    tach frontend

--- VPointApp/src/models/user.js
+++
@@ -1,8 +0,0 @@
-export default class User {
-    constructor(username, email, password, fullName) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
-        this.fullName = fullName;
-    }
-}
