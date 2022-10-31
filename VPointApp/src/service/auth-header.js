5d8cc2133318328caf4fff4524013c7afa82bcba
bumman <anhbumbk@gmail.com>
2022-10-31 14:10:20 +0700

    task13

--- VPointApp/src/service/auth-header.js
+++
@@ -1,9 +0,0 @@
-export default function authHeader(data) {
-    let user = JSON.parse(localStorage.getItem('user'));
-    if (user && user.token) {
-        return { Authorization: 'Bearer ' + user.token,
-            data:data};
-    } else {
-        return '';
-    }
-}
